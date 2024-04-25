befbbd3a95af3e1b35c2723f9ce2b5b7eb81ee40
Ki11akrokBg <kikouzunow@gmail.com>
2024-04-25 17:41:56 +0300

    Implement a custom drop-down menu for customization reasons

--- components/RegionFilter.tsx
+++ components/RegionFilter.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import styles from '../styles/RegionFilter.module.scss';
+
 
 interface RegionFilterProps {
   onFilter: (region: string) => void;
@@ -7,15 +9,50 @@ interface RegionFilterProps {
 const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']; 
 
 const RegionFilter: React.FC<RegionFilterProps> = ({ onFilter }) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState('Filter by Region');
+  const dropdownRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleSelectRegion = (region) => {
+    setSelectedRegion(region);
+    onFilter(region);
+    setIsOpen(false);
+  };
+
   return (
-    <select onChange={(e) => onFilter(e.target.value)} defaultValue="">
-      <option value="">Filter by Region</option>
-      {regions.map((region) => (
-        <option key={region} value={region}>
-          {region}
-        </option>
-      ))}
-    </select>
+    <div className={styles['region-filter']} ref={dropdownRef}>
+      <div className={styles['selected-region']} onClick={() => setIsOpen(!isOpen)}>
+        {selectedRegion}
+        <span className={styles['arrow']}>{isOpen ? '⌃' : '⌵'}</span>
+      </div>
+      {isOpen && (
+        <div className={styles['region-list']}>
+          {regions.map((region) => (
+            <div
+              key={region}
+              className={styles['region-item']}
+              onClick={() => handleSelectRegion(region)}
+            >
+              {region}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
