7d0ad92561246a015f86a0926f2597fbe88ed13e
Ki11akrokBg <kikouzunow@gmail.com>
2024-04-26 10:03:35 +0300

    Fix API call for fetching country details by name

--- utils/api.ts
+++ utils/api.ts
@@ -12,6 +12,9 @@ export const fetchCountries = async () => {
 //fetch countries by countryCode
 
 export const fetchCountryDetailsByName = async (countryName) => {
+
+  if (!countryName) return null;
+
   const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
   if (!response.ok) {
     throw new Error('Country not found');
